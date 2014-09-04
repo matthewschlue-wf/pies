992262815e91866c6ba71760e199bfd57dd5900a
Matthew Schlue <matthew.schlue@webfilings.com>
2014-09-03 22:23:37 -0500

    update hook test

--- pies/util.py
+++ pies/util.py
@@ -3,7 +3,7 @@ def say_hello():
     """Simple Test Method"""
     print "Hello World!"
     print "hello again"
-    print "hello release test!"
+    print "hello release test 2!"
 
 def say_goodbye():
     print "Goodbye cruel world!"
