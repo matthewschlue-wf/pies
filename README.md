bbc703983425e07c33766da3ec07ed81c0c8e5f6
Matthew Schlue <matthew.schlue@webfilings.com>
2015-08-24 13:38:04 -0500

    bad files

--- README.md
+++
@@ -1,6 +0,0 @@
-pies and cakes
-====
-
-I like tasty pies. Do you?
-YES YES I DO
-KEvin is bad at ping pong
--- pies/util.py
+++
@@ -1,9 +0,0 @@
-
-def say_hello():
-    """Simple Test Method"""
-    print "Hello World!"
-    print "hello again"
-    print "hello release 1462 test!"
-
-def say_goodbye():
-    print "Goodbye cruel world!"
